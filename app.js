b63e9d99f60f1db56f3f021bf4fa5fe836e715e5
bryanidem <bryan.medina.per@gmail.com>
2024-10-08 09:25:20 -0600

    Add insertRow to table to display books

--- app.js
+++ app.js
@@ -1,18 +1,67 @@
+const bookForm = document.getElementById("bookForm");
+const bookTable = document.getElementById("bookTable");
+
+let myLibrary = [];
+
 class Book {
-  constructor(title, author, pages, read) {
+  constructor({ title, author, read }) {
     this.title = title;
     this.author = author;
-    this.pages = pages;
     this.read = read;
-
-    this.info = () => {
-      return `${this.title} by ${this.author} has ${this.pages} ${
-        this.read ? "read" : "not read yet"
-      }`;
-    };
   }
 }
 
-const hobbit = new Book("zelda", "nintendo", 123, true);
+const addBookToLibrary = (bookData) => {
+  const book = new Book(bookData);
+  myLibrary = [...myLibrary, book];
+};
 
-console.log(hobbit.info());
+const insertRow = (book) => {
+  const row = document.createElement("tr");
+
+  const titleCell = document.createElement("td");
+  titleCell.textContent = book.title;
+
+  const authorCell = document.createElement("td");
+  authorCell.textContent = book.author;
+
+  const readCell = document.createElement("td");
+  readCell.textContent = book.read;
+
+  const deleteCell = document.createElement("td");
+  const deleteButton = document.createElement("button");
+  deleteButton.className = "deleteButton";
+  deleteButton.textContent = "Delete";
+  deleteCell.appendChild(deleteButton);
+
+  row.appendChild(titleCell);
+  row.appendChild(authorCell);
+  row.appendChild(readCell);
+  row.appendChild(deleteButton);
+
+  bookTable.appendChild(row);
+};
+
+const handleBookSubmit = (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const bookData = Object.fromEntries(formData);
+
+  if (!bookData.title || !bookData.author) {
+    alert("Please fill all the required fields");
+    return;
+  }
+
+  addBookToLibrary(bookData);
+  bookForm.reset();
+  console.log(myLibrary);
+  renderBooks(myLibrary);
+};
+
+const renderBooks = (myLibrary) => {
+  myLibrary.map((book) => {
+    insertRow(book);
+  });
+};
+
+bookForm.addEventListener("submit", handleBookSubmit);
